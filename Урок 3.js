1c80d8b56b574d90f00b1d321bb27f797cbbef04
Еремин Максим <96587242+zxcvb1023zxcvbzxcvb@users.noreply.github.com>
2022-04-03 21:14:27 +0400

    Add files via upload

---
+++ Урок 3.js
@@ -0,0 +1,53 @@
+let money = 5000;
+let income = 'Part-time job';
+let addExpenses = 'travel,transport, food';
+let deposit = true;
+let mission = 200000;
+let period = 1;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+console.log("Период равен "+period+" месяцев");
+console.log("Цель заработать "+mission+" рублей");
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(", "));
+let budgetDay = money;
+console.log(budgetDay);
+
+money = prompt("Ваш месячный доход?");
+addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую");
+deposit = confirm("Имеется ли у Вас депозит в банке?");
+let expenses1 = prompt("Введите обязательную статью расходов");
+let expenses2 = prompt("Введите обязательную статью расходов");
+let amount1 = prompt("Во сколько это обойдётся?");
+let amount2 = prompt("Во сколько это обойдётся?");
+console . magazine (budgetmonth);
+console . log ( Math.ceil ( mission / ( money - quantity1 - quantity2 ) ) ) ;  _
+console . log ( Math.floor ( budgetDay = budgetMonth / 30 ) ) ;  _
+if ( budgetDay === 1200 )
+{
+    console . log( 'Your income is almost high' ) ;
+}
+else if ( budgetDay === 600 )
+{
+    console . log ( 'You are almost average income' ) ;
+}
+else if ( budgetDay > 1200 )
+{
+    console . log( 'Your income is high' ) ;
+}
+else if ( budgetDay < 1200 && BudgetDay > 600 )
+{
+    console . log ( 'Your income is average' ) ;
+}
+else if ( budgetDay < 0 )
+{
+    console . log ( 'Something went wrong' ) ;
+}
+yet
+{
+    console . log('Unfortunately your income is below average' ) ;
+} ;
